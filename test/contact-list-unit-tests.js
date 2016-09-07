0677b5ad834dda691320620181061b344126559b
Jeff Duke <jeff.duke@gmail.com>
2016-09-07 16:08:47 -0600

    add-new-contact to page

---
+++ test/contact-list-unit-tests.js
@@ -0,0 +1,41 @@
+const assert = require('chai').assert;
+
+describe('our test bundle', function () {
+  it('should work', function () {
+    assert(true);
+    });
+  });
+
+describe('create a contact', function () {
+  context('createNewContact function', function () {
+
+    it.skip('should be a function', function () {
+      assert.isFunction(createNewContact);
+    });
+
+    it.skip('should create a new contact', function () {
+
+    });
+
+    context('properties of the new contact', function () {
+
+      it.skip('the new contact should have a name', function () {
+
+      });
+
+      it.skip('the new contact should have a company name', function () {
+
+      });
+
+      it.skip('the new contact should have a phone number', function () {
+
+      });
+
+      it.skip('the new contact should have a followup status', function () {
+
+      });
+
+    });
+
+  });
+});
