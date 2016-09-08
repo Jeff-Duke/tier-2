6b9705c3c739d2b453a37ce1ac6aa83c21bd5e1b
swanie21 <kswanie21@gmail.com>
2016-09-08 14:01:13 -0600

    configure react with firebase

--- webpack.config.js
+++ webpack.config.js
@@ -14,10 +14,17 @@ module.exports = {
       { test: /\.js$/, exclude: '/node_modules/', loader: 'babel-loader' },
       { test: /\.css$/, loader: 'style!css' },
       { test: /\.scss$/, loader: 'style!css!sass' },
-      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader?presets[]=es2015' }
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader?presets[]=es2015' },
+      { test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react'],
+        },
+      }
     ]
   },
   resolve: {
-    extensions: ['', '.js', '.json', '.scss', '.css']
+    extensions: ['', '.js', '.jsx', '.json', '.scss', '.css']
   }
 };
